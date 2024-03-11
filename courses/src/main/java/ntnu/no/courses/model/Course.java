d53d48577bd06b91a327e10d08a973e89a4fa3f4
birgilv <birgittevik84@gmail.com>
2024-03-11 15:58:45 +0100

    Added javadoc to Course

--- courses/src/main/java/ntnu/no/courses/model/Course.java
+++ courses/src/main/java/ntnu/no/courses/model/Course.java
@@ -13,7 +13,7 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 
 /**
- * Represents a resource: a book. We store Book objects in the application state.
+ * Represents a resource: a course. Course objects are stored in the application state.
  */
 @Entity
 public final class Course {
@@ -26,61 +26,110 @@ public final class Course {
 
     @ManyToMany(mappedBy = "courses")
     private Set<User> users = new HashSet<>();
-
+    /**
+     * Default constructor for Course.
+     */
     public Course() {
     }
 
 
+
     /**
-     * Check if this object is a valid book.
+     * Check if this object is a valid course.
      *
-     * @return True if the book is valid, false otherwise
+     * @return True if the course is valid, false otherwise
      */
     @JsonIgnore
     public boolean isValid() {
         return name != null && !name.equals("");
     }
-
+    /**
+     * Getter for the course ID.
+     *
+     * @return The ID of the course
+     */
     public int getId() {
         return id;
     }
-
+    /**
+     * Setter for the course ID.
+     *
+     * @param id The ID to set
+     */
     public void setId(int id) {
         this.id = id;
     }
-
+    /**
+     * Getter for the course name.
+     *
+     * @return The name of the course
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * Setter for the course name.
+     *
+     * @param name The name to set
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    /**
+     * Getter for the course description.
+     *
+     * @return The description of the course
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Setter for the course description.
+     *
+     * @param description The description to set
+     */
     public void setDescription(String description) {
         this.description = description;
     }
-
+    /**
+     * Getter for the course duration.
+     *
+     * @return The duration of the course
+     */
     public int getDuration() {
         return duration;
     }
-
+    /**
+     * Setter for the course duration.
+     *
+     * @param duration The duration to set
+     */
     public void setDuration(int duration) {
         this.duration = duration;
     }
-
+    /**
+     * Getter for the set of users enrolled in the course.
+     *
+     * @return The set of users enrolled in the course
+     */
     public Set<User> getUsers() {
         return users;
     }
-
+    /**
+     * Setter for the set of users enrolled in the course.
+     *
+     * @param users The set of users to set
+     */
     public void setUsers(Set<User> users) {
         this.users = users;
     }
-
+    /**
+     * Checks if two course objects are equal.
+     *
+     * @param obj The object to compare
+     * @return True if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == this) return true;
@@ -91,12 +140,20 @@ public final class Course {
                 this.description == that.description &&
                 this.duration == that.duration;
     }
-
+    /**
+     * Generates a hash code for the course object.
+     *
+     * @return The hash code of the object
+     */
     @Override
     public int hashCode() {
         return Objects.hash(id, name, description, duration);
     }
-
+    /**
+     * Generates a string representation of the course object.
+     *
+     * @return The string representation of the object
+     */
     @Override
     public String toString() {
         return "Course[" +
