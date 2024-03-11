de16d57dceab0a697d00d80291761daef99fc351
birgilv <birgittevik84@gmail.com>
2024-03-11 13:27:01 +0100

    Added the repository for course

---
+++ courses/src/main/java/ntnu/no/courses/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package ntnu.no.courses.repository;
+
+import ntnu.no.courses.model.Course;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository interface for SQL access to our database table.
+ */
+@Repository
+public interface CourseRepository extends CrudRepository<Course, Integer> {
+}
