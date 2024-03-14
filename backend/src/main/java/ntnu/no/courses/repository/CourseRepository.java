9d457578882df5480c01f0c41ea8f27fae3fb9d5
birgilv <birgittevik84@gmail.com>
2024-03-14 15:06:33 +0100

    Created the course repo

--- backend/src/main/java/ntnu/no/courses/repository/CourseRepository.java
+++ backend/src/main/java/ntnu/no/courses/repository/CourseRepository.java
@@ -1,6 +1,7 @@
 package ntnu.no.courses.repository;
 
 import ntnu.no.courses.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import org.springframework.stereotype.Repository;
  * Repository interface for SQL access to our database table.
  */
 @Repository
-public interface CourseRepository extends CrudRepository<Course, Integer> {
+public interface CourseRepository extends JpaRepository<Course,Long> {
 }
