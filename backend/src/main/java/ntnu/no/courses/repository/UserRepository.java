a9fc0d486d93abc8795a69eba6bfecd65c046229
birgilv <birgittevik84@gmail.com>
2024-03-14 15:05:08 +0100

    Created the user repo

---
+++ backend/src/main/java/ntnu/no/courses/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package ntnu.no.courses.repository;
+
+import ntnu.no.courses.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
+
