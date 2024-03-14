a44bfdc6d949be07ea4e9a2d746ead61e8e41842
birgilv <birgittevik84@gmail.com>
2024-03-14 17:34:07 +0100

    Added display for courses on webpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
-import { useState } from 'react'
 import appIcon from '/appIcon.svg'
 import './css/App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import HomePage from "./pages/HomePage";
+import { Link, useParams } from "react-router-dom";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [courses, setCourses] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    loadCourses();
+  }, []);
+
+  const loadCourses = async () => {
+    const result = await axios.get("http://localhost:8080/courses");
+    setCourses(result.data);
+  };
 
   return (
     <>
@@ -12,9 +28,28 @@ function App() {
       </div>
       <h1>Learniverse Connect</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <table className="table border shadow">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Name</th>
+              <th scope="col">Description</th>
+              <th scope="col">Duration</th>
+            </tr>
+          </thead>
+          <tbody>
+            {courses.map((course, index) => (
+              <tr>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{course.name}</td>
+                <td>{course.description}</td>
+                <td>{course.duration}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   )
